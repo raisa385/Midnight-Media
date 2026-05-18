1ece3b4fd93e55165034ec14dbbee88ae6e81dcf
raisa385 <rr.anwar385@gmail.com>
2026-05-18 18:29:24 +0600

    changed user id to type INT

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS midnightMedia_db;
 USE midnightMedia_db;
 
 CREATE TABLE users(
-    id VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     userRole ENUM('admin','moderator'),
