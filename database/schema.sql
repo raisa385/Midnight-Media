42e8232c4f9830f6ab0eabc6b2ee618955ce3d68
raisa385 <rr.anwar385@gmail.com>
2026-05-18 21:35:58 +0600

    added missing name field in schema and database.php

--- database/schema.sql
+++ database/schema.sql
@@ -3,6 +3,7 @@ USE midnightMedia_db;
 
 CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
+    name, VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     userRole ENUM('admin','moderator'),
