0289ef943642dad977f2afd97deae353af760f51
raisa385 <rr.anwar385@gmail.com>
2026-05-19 00:42:06 +0600

    added client side validation for registration

--- controllers/controlRegister.php
+++ controllers/controlRegister.php
@@ -6,35 +6,23 @@
     if($_SERVER['REQUEST_METHOD']==="POST"){
         $name=$_POST['name'];
         $email=$_POST['email'];
-        $password=$_POST['password'];
+        $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
         $userRole=$_POST['userRole'];
 
         $result=checkEmail($conn, $email);
         if($result===false){
             $_SESSION['flash_msg'] = "Email already in use. Try another.";
-            header("Location: viewRegister.php");
-            exit();
-        }
-
-        if(strlen($password)<8){
-            $_SESSION['flash_msg'] = "Password must be at least 8 characters long";
-            header("Location: viewRegister.php");
-            exit();
-        }
-        $password = password_hash($password,PASSWORD_DEFAULT);
-        
-        if($name=='' || $email=='' || $password==''|| $userRole==''){
-            $_SESSION['flash_msg'] = "Please fill in all required fields";
-            header("Location: viewRegister.php");
+            header("Location: ../views/viewRegister.php");
             exit();
         }
 
         $result= registerUser($conn,$name,$email,$password,$userRole);
         if($result===false){
             $_SESSION['flash_msg'] = "Server error: Failed to Register. Please try again.";
-            header("Location: viewRegister.php");
+            header("Location: ../views/viewRegister.php");
             exit();
         }
+        
         $_SESSION['flash_msg'] = "Registration successful. Redirecting...";
-        header("Location: viewLogin.php");
+        header("Location: ../views/viewLogin.php");
     }
--- views/viewRegister.php
+++ views/viewRegister.php
@@ -9,11 +9,12 @@
         <?php if(isset($_SESSION['flash_msg'])):?>
         <p style="color:red;"><?=htmlspecialchars($_SESSION['flash_msg']);?></p>
         <?php endif;?>
+        <p id="JS_flash_msg" style="color:red display:none;"></p>
 
-        <form action="/controlRegister.php" method="POST">
-            <input type="text" name="name" placeholder="Name" required><br>
-            <input type="email" name="email" placeholder="Email" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
+        <form id="registerForm" action="../controllers/controlRegister.php" method="POST">
+            <input type="text" id='name' name="name" placeholder="Name" required><br>
+            <input type="email" id='email'name="email" placeholder="Email" required><br>
+            <input type="password" id='password' name="password" placeholder="Password" required><br>
             <label for="role">Choose your role</label>
             <select id="userRole" name="userRole">
                 <option value="Admin">Apply for Admin Role</option>
@@ -22,5 +23,30 @@
             <button type="submit">Submit</button>
         </form>
         <p>Already registered? <a href="viewLogin.php">Login here</a></p>
+
+        <script>
+            document.getElementById('registerForm').addEventListener('submit',function(event)){
+                const name= document.getElementById('name').value.trim();
+                const email= document.getElementById('email').value.trim();
+                const password= document.getElementById('password').value.trim();
+                const userRole= document.getElementById('userRole').value.trim();
+                const message= document.getElementById('JS_flash_msg').value.trim();
+
+                if(password.length<8){
+                    event.preventDefault();
+                    message.innerText="Password length must be at least 8 characters";
+                    message.style.display='block';
+                    return;
+                }   
+        
+                if($name=='' || $email=='' || $password==''|| $userRole==''){
+                    event.preventDefault();
+                    message.innerText="Please fill all required fields";
+                    message.style.display='block';
+                    return;
+                }
+                message.style.display='none';
+            }
+        </script>
     </body>
 </html>
