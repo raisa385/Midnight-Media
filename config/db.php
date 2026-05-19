a213239c391f39389e83781d74a40e6f4f3e86a2
Raisa <rr.anwar385@gmail.com>
2026-05-20 02:24:08 +0600

    Update database connection to midnightmedia_db

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "ftp_server");
+$conn = mysqli_connect("localhost", "root", "", "midnightmedia_db");
 
 if (!$conn) {
     die("Database Connection Failed");
