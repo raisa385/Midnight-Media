4218c6d9a69b786845007be996a0bccbbb4d7ce2
Israt Jahan Singdha <ijsnigdha9@gmail.com>
2026-05-21 11:00:12 +0600

    fix bug

--- controllers/upload_content.php
+++ controllers/upload_content.php
@@ -85,7 +85,7 @@ $filePath = 'public/uploads/contents/' . $filename;
 $stmt = mysqli_prepare($conn,
     "INSERT INTO contents (title, description, file_path, category_id, uploader_id)
      VALUES (?, ?, ?, ?, ?)");
-mysqli_stmt_bind_param($stmt, 'sssii', $title, $description, $filePath, $category_id, $uploader_id);
+mysqli_stmt_bind_param($stmt, 'sssii', $title, $description, $filePath, $category-id, $uploader_id);
 mysqli_stmt_execute($stmt);
 
 $_SESSION['flash'] = "'{$title}' uploaded successfully.";
